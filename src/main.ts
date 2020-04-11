cd04a59c777601efb475dc3a00ca115be4b353ad
kewin <404502753@qq.com>
2020-04-11 23:55:23 +0800

    配置静态资源

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { NestExpressApplication } from '@nestjs/platform-express'
+import { join } from 'path' // 方法二 需要引入的文件
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  // 方法一:
+  // http://localhost:3000/love.jpg 可访问
+  app.useStaticAssets('public',{
+    // http://localhost:3000/static/love.jpg 可访问
+    prefix:'/static/' // 配置虚拟路径
+  }) // 配置静态资源目录
+
+  // 方法二:
+  // app.useStaticAssets(join(__dirname, '..', 'public'), {
+  //   // http://localhost:3000/static/love.jpg 可访问
+  //   prefix: '/static/' // 配置虚拟路径
+  // }) // 配置静态资源目录
   await app.listen(3000);
 }
 bootstrap();
