4df64d2aaf80c28fb3e6b5864d3f4bddda9c10a1
kewin <404502753@qq.com>
2020-04-12 12:19:35 +0800

    对cookie进行加密

--- src/main.ts
+++ src/main.ts
@@ -23,9 +23,12 @@ async function bootstrap() {
   // 配置模板引擎的基本目录(存放模板引擎的路径)
   app.setBaseViewsDir('views')
   app.setViewEngine('ejs')
-  // 通过中间件的方式使用cookieparser ---- 和express使用方法一直
-  app.use(CookieParser())
-  
+  // 通过中间件的方式使用cookieparser ---- 和express使用方法一致
+  // 不传参代表不对cookie进行加密
+  // app.use(CookieParser())
+  // 传任意参数对cookie进行加密 CookieParser会对参数进行算法加密 此时获取和设置cookie的方法需要添加新的参数否则无法生效
+  app.use(CookieParser('encrypet cookie'))
+
   await app.listen(3000);
 }
 bootstrap();
--- src/setcookie/setcookie.controller.ts
+++ src/setcookie/setcookie.controller.ts
@@ -7,19 +7,34 @@ export class SetcookieController {
   // 2.注入服务
   constructor(private setCookie: SetcookieService) { }
 
-  // 设置cookie
+  // 设置cookie(不加密)
   @Get()
   index(@Response() res) {
     // maxAge过期时间
     // httpOnly 默认false 不允许客户端脚本访问
     res.cookie('username', 'xiaobai', { maxAge: 1000 * 60, httpOnly: true })
-    res.send('这是cookie')
+    // 这里不能通过return给页面发送数据。但是可通过res.send()
+    res.send('这是cookie(不加密)')
   }
 
-  // 其他页面也可以使用存储的cookie
+  // 其他页面也可以使用存储的cookie(不加密)
   @Get('add')
   addIndex(@Request() req) {
     console.log(req.cookies.username)
-    return req.cookies.username
+    return req.cookies.username + ' cookie(不加密)'
+  }
+
+  // 设置cookie时进行加密
+  @Get('encrypt')
+  encryptCookie(@Response() res) {
+    res.cookie('password', 'encrypt cookie  哈哈哈', { maxAge: 1000 * 60 * 60, httpOnly: true, signed: true })
+    res.send('已对cookie进行加密,f12可查看')
+  }
+
+  // 获取加密后的cookie
+  @Get('getEncryCookie')
+  getEncryCookie(@Request() req) {
+    console.log(req.signedCookies.password)
+    return req.signedCookies.password ? `${req.signedCookies.password},是加密后的cookie` : 'cookie已加密无法访问'
   }
 }
