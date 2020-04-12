fc0845c2f87ed33efec2de744f33e9197f721097
kewin <404502753@qq.com>
2020-04-12 11:41:52 +0800

    任意控制器中都可以是用服务

--- src/shop/shop.controller.ts
+++ src/shop/shop.controller.ts
@@ -1,7 +1,12 @@
 import { Controller, Get, Post, Body, Response, Render } from '@nestjs/common';
+// 1.服务引入
+import { ServernewsService } from '../servernews/servernews.service'
 
 @Controller('shop')
 export class ShopController {
+  // 2.注入服务
+  constructor(private servernews: ServernewsService) { }
+
   @Get()
   @Render('default/shop')
   index() {
@@ -14,4 +19,9 @@ export class ShopController {
     res.redirect('/shop') // 重定向到shop页面
   }
 
+  @Get('other')
+  otherShop() {
+    // 使用
+    return this.servernews.allserverNews()
+  }
 }
