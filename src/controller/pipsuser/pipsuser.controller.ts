b433a100777f70eba9043b79a204feb3f2df4902
kewin <404502753@qq.com>
2020-04-13 21:54:38 +0800

    管道验证路由demo

--- src/controller/pipsuser/pipsuser.controller.ts
+++ src/controller/pipsuser/pipsuser.controller.ts
@@ -1,11 +1,17 @@
 import { Controller, Get, UsePipes, Query } from '@nestjs/common';
-import { UserPipe } from '../../pipe/user.pipe'
+import * as Joi from '@hapi/joi'
 
+import { UserPipe } from '../../pipe/user.pipe'
+let userSchema = Joi.object().keys({
+  name: Joi.string().required(),
+  age: Joi.number().integer().min(6).max(66).required(),
+})
 @Controller('pipsuser')
 export class PipsuserController {
+
   @Get()
   // 使用管道修饰器来实例化管道
-  @UsePipes(new UserPipe())
+  @UsePipes(new UserPipe(userSchema))
   index(@Query() info) {
     console.log(info)
   }
--- src/pipe/user.pipe.ts
+++ src/pipe/user.pipe.ts
@@ -1,11 +1,25 @@
 import { ArgumentMetadata, Injectable, PipeTransform } from '@nestjs/common';
-
+import * as Joi from '@hapi/joi'
 @Injectable()
 export class UserPipe implements PipeTransform {
+  private schema;
+  constructor(schema) {
+    this.schema = schema
+  }
   transform(value: any, metadata: ArgumentMetadata) {
+    // console.log(Joi)
+    // http://localhost:3000/pipsuser?name=zhangsan false
+
+    // http://localhost:3000/pipsuser?name=zhangsan&age=20 true
+    const { error } = this.schema.validate(value)
     // value 可以理解为get/post请求传递的值
     // metadata 可以理解为一种类型
-    console.log(value,'管道')
+    console.log(value, '管道')
+    // console.log(error)
+    // 验证不通过时返回
+    if (error) {
+      return { 'success': false }
+    }
     return value;
   }
 }
